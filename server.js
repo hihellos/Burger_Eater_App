36cb0823c5066d37b34e425d4329d9808fb1f453
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-26 18:16:16 -0400

    adding process instructions to each file

---
+++ server.js
@@ -0,0 +1,7 @@
+// Make a package.json file by running npm init from the command line.
+// Install the Express npm package: npm install express.
+// Create a server.js file.
+// Install the Handlebars npm package: npm install express-handlebars.
+// Install MySQL npm package: npm install mysql.
+// Require the following npm packages inside of the server.js file:
+    // express
