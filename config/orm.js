6d4de7e4a78a8b1d9923ec052bef3de6e0b80d21
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-27 22:18:59 -0400

    fixed db name in connection, set up orm file with select, insert and update methods

--- config/orm.js
+++ config/orm.js
@@ -4,3 +4,63 @@
     // insertOne()
     // updateOne()
 // Export the ORM object in module.exports.
+
+const connection = require("../config/connection.js");
+const { query } = require("express");
+
+// Helper function to create array of ?s then turn them into a string for sql query
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+let orm = {
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM" + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    create: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log("queryStrng: ", queryString);
+
+        connection.query(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    update: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
