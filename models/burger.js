9c8d1d3cd96bfd943f9c5e9002ab4ea63542186d
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-28 19:04:41 -0400

    worte burger model code that will call the ORM functions using burger specific input for the orm

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,25 @@
 // Inside burger.js, import orm.js into burger.js
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 // Export at the end of the burger.js file.
+
+const orm = require("../config/orm.js");
+
+let burger = {
+    all: function(cb) {
+        orm.all("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(objColVals, condiiton, cb) {
+        orm.update("burgers", objColVals, condiiton, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
