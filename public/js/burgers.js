691776b037f8db4e3786068bd9ad1593008719d7
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-29 19:54:17 -0400

    added burgers.js script for click functions, corrected file path in main

---
+++ public/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      let id = $(this).data("id");
+      let newDevour = $(this).data("newdevour");
+  
+      var newDevourState = {
+        sleepy: newDevour
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      let newBurger = {
+        name: $("#burg").val().trim(),
+        sleepy: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
