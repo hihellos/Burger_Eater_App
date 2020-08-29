b40e07ce3a2d6aea6faff9b65dde865b2f659fcd
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-28 20:00:57 -0400

    set up controller with router for app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,46 @@
     // Express
     // burger.js
 // Create the router for the app, and export the router at the end of your file.
+
+const express = require("express");
+const burger = require("burger");
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log("hbsObject: ", hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
