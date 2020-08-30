fc925726382740869252449a9b2a97c7f0b55c54
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-29 23:21:20 -0400

    added process.env.JAWSBD_URL

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.MYSQL_KEY,
-    database: "burgers_db"
-  });
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.MYSQL_KEY,
+        database: "burgers_db"
+    });
+};
 
 // Make connection
 connection.connect(function(err) {
