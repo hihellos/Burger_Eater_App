d15c4a3f679cb15f1a282c4e689f91d96a086a67
hihello <13111181+hihellos@users.noreply.github.com>
2020-08-29 21:58:24 -0400

    fixed column name in schema so data could be pulled correctly

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Junior Cheeseburger', true);
-INSERT INTO burgers (burger_name) VALUES ('Katsuburger');
-INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Junior Cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('Katsuburger');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
